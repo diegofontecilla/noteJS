ba022972bdc95cd377f5f2e532bf2da5ba83b415
Khie Biggs <khiebigzy@gmail.com>
2018-06-27 14:48:44 +0100

    NoteListView only displays the first 20 characters of a note

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -6,4 +6,11 @@
 
    let expectedText = "<ul><li><div>test note 1</div></li><li><div>test note 2</div></li></ul>";
    assert.isTrue('NoteListView returns html given notes inputted', notelistview.text() === expectedText);
+
+   notelist = new NoteListModel();
+   notelist.add('123456789012345678901');
+   notelistview = new NoteListView(notelist);
+
+   expectedText = "<ul><li><div>12345678901234567890</div></li></ul>";
+   assert.isTrue('NoteListView only displays the first 20 characters of a note', notelistview.text() === expectedText);
 })();
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,18 +1,24 @@
 'use strict';
 (function (exports) {
-   function NoteListView(noteList){
-      this._html = "<ul>";
 
-      for(let i in noteList.view()){
-         if(noteList.view().length === 0) {
-            break;
-         }
-         this._html = this._html + "<li><div>" + noteList.view()[i].text() + "</div></li>";
-      }
+    function reduceLength(text){
+        return text.substr(0,20);
+    }
 
-      this._html = this._html + "</ul>";
-   }
-   exports.NoteListView = NoteListView;
+    function NoteListView(noteList){
+        this._html = "<ul>";
+
+        for(let i in noteList.view()){
+            if(noteList.view().length === 0) {
+                break;
+            }
+
+            this._html = this._html + "<li><div>" + reduceLength(noteList.view()[i].text()) + "</div></li>";
+        }
+
+        this._html = this._html + "</ul>";
+    }
+    exports.NoteListView = NoteListView;
 })(this);
 
 NoteListView.prototype.text = function () {
