86d2991232de74a65535b908258fdd3168b61967
Khie Biggs <khiebigzy@gmail.com>
2018-06-28 16:51:25 +0100

    clicking on a link displays the note without having to refresh the page

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,13 +1,34 @@
 'use strict';
 (function (exports) {
     function NoteController(noteListModel){
-        // this._view = new NoteListView(noteListModel);
         this.notelistmodel = noteListModel;
     }
+
+    function listenForChange(notelist){
+        window.addEventListener("hashchange", function(){
+            showNote(notelist);
+        });
+    }
+
+    function showNote(notelist){
+        let noteID = getNoteID(window.location);
+        let note = notelist.view()[noteID-1];
+        let noteview = new SingleNoteView(note);
+        document.getElementById("app").innerHTML = noteview.text();
+    }
+
+    function getNoteID(location){
+        return location.hash.split('#')[1];
+    }
+
+    NoteController.prototype.update = function (element) {
+        this._view = new NoteListView(this.notelistmodel);
+        element.innerHTML = this._view.text();
+        listenForChange(this.notelistmodel);
+    };
+
     exports.NoteController = NoteController;
 })(this);
 
-NoteController.prototype.update = function (element) {
-    this._view = new NoteListView(this.notelistmodel);
-    element.innerHTML = this._view.text();
-};
+
+
